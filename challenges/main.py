18eda323ca6ad094a0ba226449cfdff07d50412a
pwokoma69-ship-it <pwokoma69@gmail.com>
2026-09-07 10:09:42 +0100

    Complete: Words Length Lookup

---
+++ challenges/main.py
@@ -0,0 +1,8 @@
+def word_lengths(words):
+    lengths = {}
+    for word in words:
+        lengths[word] = len(word)
+    return lengths
+    # TODO: loop through `words` and populate `lengths` with word -> length of word
+
+    return lengths

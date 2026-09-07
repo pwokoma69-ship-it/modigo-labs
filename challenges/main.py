5c842dedcb4400e118a39735abb8aa91ab5118b7
pwokoma69-ship-it <pwokoma69@gmail.com>
2026-09-07 11:25:48 +0100

    Complete: Inventory Counter

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,10 @@
-def word_lengths(words):
-    lengths = {}
-    for word in words:
-        lengths[word] = len(word)
-    return lengths
-    # TODO: loop through `words` and populate `lengths` with word -> length of word
-
-    return lengths
+def count_items(items):
+    counts = {}
+    for item in items:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+    return counts
+    # TODO: use a for loop to build a dictionary counting each item in `items`
+    pass

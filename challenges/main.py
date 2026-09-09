f70ce86be68b0afd18159602ee8b273b09afa243
pwokoma69-ship-it <pwokoma69@gmail.com>
2026-09-09 12:43:21 +0100

    Complete: Common Elements Finder

--- challenges/main.py
+++ challenges/main.py
@@ -1,10 +1,10 @@
-def count_items(items):
-    counts = {}
-    for item in items:
-        if item in counts:
-            counts[item] += 1
-        else:
-            counts[item] = 1
-    return counts
-    # TODO: use a for loop to build a dictionary counting each item in `items`
+def find_common_elements(list1, list2):
+    common = []
+    for item in list1:
+        for value in list2:
+            if item == value:
+                if item not in common:
+                    common.append(item)
+    return common
+    # TODO: use for loops to find values present in both list1 and list2, with no duplicates
     pass

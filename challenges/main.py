3beaf4552a8ad76da71344c62845a16e53221492
pwokoma69-ship-it <pwokoma69@gmail.com>
2026-09-10 12:05:35 +0100

    Complete: Fix the Tag Merger

--- challenges/main.py
+++ challenges/main.py
@@ -1,4 +1,3 @@
-def same_point(point1, point2):
-    return point1 == point2
-    # TODO: return True if point1 and point2 represent the same location
-    pass
+def merge_tags(tags1, tags2):
+    merged = tags1 | tags2
+    return merged

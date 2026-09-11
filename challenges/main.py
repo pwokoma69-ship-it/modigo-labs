06cbcf0b459c49753e0fe6e8175a4e8fbfae63c2
pwokoma69-ship-it <pwokoma69@gmail.com>
2026-09-11 10:25:01 +0100

    Complete: Deduplicate While Preserving Order

--- challenges/main.py
+++ challenges/main.py
@@ -1,4 +1,11 @@
-def count_unique_visitors(visitors):
-    return len(set(visitors))
-    # TODO: convert `visitors` to a set to remove duplicates, then return its length
+def dedupe_preserve_order(items):
+    total = []
+    for item in items:
+        if item not in total:
+            total.append(item)
+    return total
+
+
+    # TODO: use a set to track seen values while building a new list
+    # that preserves the original order of first appearances
     pass
